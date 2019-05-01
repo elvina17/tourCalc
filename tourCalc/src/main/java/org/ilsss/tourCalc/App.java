9dd71b82ec9c2e656ff72adac1941f4627cc18a9
ilsss <galija1998@rambler.ru>
2019-05-02 00:15:01 +0300

    Первоначальная загрузка проекта

---
+++ tourCalc/src/main/java/org/ilsss/tourCalc/App.java
@@ -0,0 +1,13 @@
+package org.ilsss.tourCalc;
+
+/**
+ * Hello world!
+ * One’s greatest challenge is to control oneself
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
